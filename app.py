0008ace4453e40a5490daab5089deabe37235dc9
Kale <kyle.brooks@csedge.org>
2022-11-19 15:44:32 -0500

    commit 2

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
-from sense_hat import SenseHat 
 
-sense = SenseHat()
+from gpiozero import LED
+from time import sleep
 
+
+    
 app = Flask(__name__)
+
+led = LED(17)
 
 @app.route('/')
 def index():
@@ -13,17 +17,15 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/message', methods=['POST'])
+@app.route('/animation', methods=['POST'])
 def post_message():
-    message = request.form['message']
-    name = request.form['name']
-    display = f'{message}  Love, {name}'
-    sense.show_message(display, text_colour=[0, 0, 255])
-    return render_template('received.html')
-    if message == "lol":
-        display = 'no'
-        print(display)
-        #return render_template('redir.html')
+    animation = request.form['dropdown']
+    if animation == "on":
+        led.on()
+    elif animation == "off":
+        led.off()
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
